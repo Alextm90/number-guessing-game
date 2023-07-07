f8d06b3511f205ed80ec3e25df1c3592137d93a9
Alex Miserandino <amiserandino20@gmail.com>
2023-07-07 11:23:17 -0700

    Update number_guess.sql

--- number_guess.sql
+++ number_guess.sql
@@ -90,13 +90,15 @@ ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.u
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.users VALUES (2, 'dsfghdf', 1, 2);
+INSERT INTO public.users VALUES (1, 'dfg', 2, 1);
 
 
 --
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_user_id_seq', 2, true);
 
 
 --
@@ -118,4 +120,5 @@ ALTER TABLE ONLY public.users
 --
 -- PostgreSQL database dump complete
 --
+
 
