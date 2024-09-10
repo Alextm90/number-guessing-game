4f712018b50b0dd906d8f4846d75bdadb9a9364c
Alex Miserandino <amiserandino20@gmail.com>
2024-09-09 21:48:23 -0400

    updated username

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
- PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+ #!/bin/bash
+PSQL="psql --username=alextm90 --dbname=number_guess -t --no-align -c"
+
 # create random number
 RAN_NUMBER=$((1 + $RANDOM % 1000))
 echo $RAN_NUMBER
